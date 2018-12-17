9e3b857715d45e6b45411209f2c06863689c0179
Soni <Soni>
2018-12-17 17:13:51 +0530

    movie

--- Controller.js
+++ Controller.js
@@ -3,31 +3,14 @@ const Response = require('./models/responseSchema');
 
 Controller.getResponse = (req, res) => {
     if(req.body.queryResult.intent.displayName == "Movie"){
-        const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
-    http.get(reqUrl, (responseFromAPI) => {
-        let completeResponse = '';
-        responseFromAPI.on('data', (chunk) => {
-            completeResponse += chunk;
+        Response.findOne({service:req.body.queryResult.intent.displayName},(err, data) => {
+            if(err){
+                res.json("Oh oo, Something went wrong");
+            }
+            else{
+                res.json(data.response);
+            }
         });
-        responseFromAPI.on('end', () => {
-            const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-
-            return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
-                source: 'get-movie-details'
-            });
-        });
-    }, (error) => {
-        return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
-            source: 'get-movie-details'
-        });
-    });
     }
     if(req.body.queryResult.intent.displayName == "Cab"){
         Response.findOne({service:req.body.queryResult.intent.displayName},(err, data) => {
