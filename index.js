a448aee51cd340eb9d502aaf8f5bee9995bd2d60
Soni <Soni>
2018-12-18 14:05:48 +0530

    ejs final working fine.

--- index.js
+++ index.js
@@ -21,6 +21,51 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+app.get("/cabs_view", (req, res) => {
+    res.render("pages/cabs_view");
+});
+app.get("/drivers_view", (req, res) => {
+    res.render("pages/drivers_view");
+});
+
+app.get("/flights_view", (req, res) => {
+    res.render("pages/flights_view");
+});
+app.get("/food_delivery_view", (req, res) => {
+    res.render("pages/food_delivery_view");
+});
+app.get("/handyman_view", (req, res) => {
+    res.render("pages/handyman_view");
+});
+app.get("/movies_view", (req, res) => {
+    res.render("pages/movies_view");
+});
+
+
+app.get("/cabs_edit", (req, res) => {
+    res.render("pages/cabs_edit");
+});
+app.get("/drivers_edit", (req, res) => {
+    res.render("pages/drivers_edit");
+});
+app.get("/flights_edit", (req, res) => {
+    res.render("pages/flights_edit");
+});
+app.get("/food_delivery_edit", (req, res) => {
+    res.render("pages/food_delivery_edit");
+});
+app.get("/handyman_edit", (req, res) => {
+    res.render("pages/handyman_edit");
+});
+app.get("/movies_edit", (req, res) => {
+    res.render("pages/movies_edit");
+});
+
+
+
+
+
 app.post("/api", Controller.getResponse);
 app.post("/add", Controller.saveResponse);
 
